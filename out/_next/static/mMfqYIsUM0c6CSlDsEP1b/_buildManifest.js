dca7f6d2872c360ac0ea45c82972f9087575ba36
slozero10 <slotzero10@gmail.com>
2024-04-24 14:41:04 +0800

    update

---
+++ out/_next/static/mMfqYIsUM0c6CSlDsEP1b/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9bf4967b90cfe74b.1713940637171.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- out/_next/static/rH59B-rEE6i5exFY-_cNY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-9bf4967b90cfe74b.1713910000663.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
